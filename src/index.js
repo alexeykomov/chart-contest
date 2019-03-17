ef74bdcbaa79da225f24a8eed38e653b06e48c23
alexeykomov <alexeykofficial@gmail.com>
2019-03-17 18:24:25 +0300

    add: first working version

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@
  * @author alexeykcontact@gmail.com (Alex K)
  */
 
-import { data } from './data';
-
-const chart = new Chart(0);
-window.addEventListener('DOMContentLoaded', () => chart.onLoad(), false);
-window.addEventListener('unload', () => chart.onUnLoad(), false);
+import { data, Data, DataEntry } from './data.js';
 
 class Chart {
   /**
@@ -26,25 +22,39 @@ class Chart {
     this.buttonOk = null;
     /** @type {HTMLButtonElement} */
     this.buttonCancel = null;
+
+    /** @type {Element} */
+    this.gripLeft = null;
+    /** @type {Element} */
+    this.gripRight = null;
+    /** @type {Element} */
+    this.glassLeft = null;
+    /** @type {Element} */
+    this.glassRight = null;
+    /** @type {Element} */
+    this.window = null;
+
     /** @type {CanvasRenderingContext2D} */
     this.context = null;
     /** @type {CanvasRenderingContext2D} */
     this.contextLegend = null;
     this.chartNumber = chartNumber;
+    this.dpr = window.devicePixelRatio || 1;
 
     this.state = {
+      shouldRender: true,
+
       mainCanvasWidth: 0,
       mainCanvasHeight: 100,
       legendCanvasWidth: 0,
       legendCanvasHeight: 100,
 
-      linesMain: [],
-      linesLegend: [],
+      lines: [],
 
-      minXMain: 0,
-      maxXMain: 0,
-      minYMain: 0,
-      maxYMain: 0,
+      minX: 0,
+      maxX: 0,
+      minY: 0,
+      maxY: 0,
 
       minXLegend: 0,
       maxXLegend: 0,
@@ -53,16 +63,26 @@ class Chart {
 
       colors: [],
     };
+    console.log('data[chartNumber]: ', data[chartNumber]);
     this.formatDataEntry(this.state, data[chartNumber]);
+    this.setExtremums(this.state);
   }
 
   onLoad() {
-    this.mainCanvas = document.querySelector(
-      `main:nth-child(${this.chartNumber}) .main-canvas`,
-    );
-    this.legendCanvas = document.querySelector(
-      `main:nth-child(${this.chartNumber}) .legend-canvas`,
-    );
+    this.mainCanvas = /** @type {HTMLCanvasElement} */ (document.querySelectorAll(
+      '.main-canvas'
+    )[this.chartNumber]);
+    this.legendCanvas = /** @type {HTMLCanvasElement}*/ (document.querySelectorAll(
+      '.legend-canvas'
+    )[this.chartNumber]);
+    this.gripLeft = document.querySelectorAll('.grip-left')[this.chartNumber];
+    this.gripRight = document.querySelectorAll('.grip-right')[this.chartNumber];
+    this.glassLeft = document.querySelectorAll('.glass-left')[this.chartNumber];
+    this.glassRight = document.querySelectorAll('.glass-right')[
+      this.chartNumber
+    ];
+    this.window = document.querySelectorAll('.window')[this.chartNumber];
+
     this.context = this.setupCanvas(this.mainCanvas);
     this.contextLegend = this.setupCanvas(this.legendCanvas);
 
@@ -71,24 +91,70 @@ class Chart {
     this.state.legendCanvasWidth = this.legendCanvas.width;
     this.state.legendCanvasHeight = this.legendCanvas.height;
 
-    this.maxXMain = this.mainCanvas.width;
-    this.maxYMain = this.mainCanvas.height;
-    this.maxXLegend = this.legendCanvas.width;
-    this.maxYLegend = this.legendCanvas.height;
+    this.state.legendWidth = this.state.legendCanvasWidth / this.dpr;
+    this.state.gripWidth = +/\d+/.exec(
+      window.getComputedStyle(this.gripLeft)['width']
+    )[0];
+
+    this.attachEvents();
 
     this.render();
   }
 
+  attachEvents() {
+    this.gripLeft.addEventListener(
+      'mousedown',
+      e => {
+        this.dragTarget = e.currentTarget;
+        let rect = this.legendCanvas.getBoundingClientRect();
+        this.dragLeft = rect.left;
+        this.dragRight = rect.right;
+        this.dragWidth = rect.width;
+      },
+      false
+    );
+
+    this.gripLeft.addEventListener('mousedown', e => {}, false);
+
+    this.window.addEventListener('mousedown', e => {}, false);
+
+    document.addEventListener('mousemove', e => {
+      if (this.dragTarget) {
+        const pageX = e.pageX;
+        console.log('pageX: ', pageX);
+        const relX =
+          pageX >= this.dragRight
+            ? this.dragRight
+            : pageX <= this.dragLeft
+            ? 0
+            : pageX - this.dragLeft;
+        console.log('relX: ', relX);
+        this.state.minX =
+          this.state.minXOrig +
+          ((this.state.maxXOrig - this.state.minXOrig) * relX) / this.dragWidth;
+      }
+    });
+    document.addEventListener('mouseup', e => {
+      this.dragTarget = null;
+    });
+  }
+
   /**
-   * @type {HTMLCanvasElement} canvasEl
+   * @param {HTMLCanvasElement} canvasEl
    * @return {CanvasRenderingContext2D}
-   * */
+   */
   setupCanvas(canvasEl) {
-    const context = canvasEl.getContext('2d');
-    const dpr = window.devicePixelRatio || 1;
-    context.scale(dpr, dpr);
+    const context = /** @type {CanvasRenderingContext2D} */ (canvasEl.getContext(
+      '2d'
+    ));
+    let computedStyle = window.getComputedStyle(canvasEl);
+    canvasEl.width = +/\d+/.exec(computedStyle['width'])[0] * this.dpr;
+    canvasEl.height = +/\d+/.exec(computedStyle['height'])[0] * this.dpr;
+    context.scale(this.dpr, this.dpr);
     return context;
   }
+
+  setupWindow() {}
 
   onUnLoad() {}
 
@@ -98,59 +164,72 @@ class Chart {
    * @return {State}
    */
   formatDataEntry(state, entry) {
-    const linesMain = [];
-    const linesLegend = [];
-    entry.columns.forEach(col => {
-      linesMain.push(col.slice(1));
-      linesLegend.push(col.slice(1));
+    const lines = [];
+    entry['columns'].forEach(col => {
+      lines.push(col.slice(1));
     });
-    const colors = Object.keys(entry.colors).map(key => entry.colors[key]);
+    const colors = Object.keys(/** @type {!Object}*/ (entry['colors'])).map(
+      key => entry['colors'][key]
+    );
 
-    state.linesMain = linesMain;
-    state.linesLegend = linesLegend;
+    state.lines = lines;
     state.colors = colors;
 
     return state;
   }
 
-  normalizeLines() {
-    this.normalizeEntry(
-      this.state.mainCanvasWidth,
-      this.state.mainCanvasHeight,
-      this.state.linesMain,
-    );
-    this.normalizeEntry(
-      this.state.mainCanvasWidth,
-      this.state.mainCanvasHeight,
-      this.state.linesMain,
-    );
+  /**
+   * @param {State} state
+   * @return {State} state
+   */
+  setExtremums(state) {
+    const lines = state.lines;
+    const minX = lines[0][0];
+    const maxX = lines[0][lines[0].length - 1];
+    const maxY = lines
+      .slice(1)
+      .reduce((max, line) => Math.max(max, Math.max(...line)), -Infinity);
+    const minY = lines
+      .slice(1)
+      .reduce((max, line) => Math.min(max, Math.min(...line)), +Infinity);
+
+    state.minX = minX;
+    state.maxX = maxX;
+    state.minY = minY;
+    state.maxY = maxY;
+
+    state.minXOrig = minX;
+    state.maxXOrig = maxX;
+    state.minYOrig = minY;
+    state.maxYOrig = maxY;
+
+    return state;
   }
 
   /**
+   * @param {State} state
    * @param {number} canvasWidth
    * @param {number} canvasHeight
-   * @param {Array.<Array<number>>} lines
-   * @return {Array.<Array.<number>>}
+   * @param {boolean} useOrig
+   * @return {!Array.<!Array.<number>>}
    */
-  normalizeEntry(canvasWidth, canvasHeight, lines) {
-    const minX = lines[0][0];
-    const maxX = lines[0][lines.length - 1];
-    const maxY = lines
-      .slice(1)
-      .reduce((max, line) => Math.max(max, Math.max.apply(...line)), -Infinity);
-    const minY = lines
-      .slice(1)
-      .reduce((max, line) => Math.min(max, Math.min.apply(...line)), +Infinity);
+  normalizeEntry(state, canvasWidth, canvasHeight, useOrig) {
+    const lines = state.lines;
+    const minX = useOrig ? state.minXOrig : state.minX;
+    const maxX = useOrig ? state.maxXOrig : state.maxX;
+    const maxY = useOrig ? state.maxYOrig : state.maxY;
+    const minY = useOrig ? state.minYOrig : state.minY;
 
-    return lines.map(vector => {
-      return [
-        this.normalizeValue(vector[0], minX, maxX, 0, canvasWidth, true),
-      ].concat(
-        vector
-          .slice(1)
-          .map(v =>
-            this.normalizeValue(vector[0], minX, maxX, 0, canvasWidth, false),
-          ),
+    return lines.map((line, index) => {
+      if (index === 0) {
+        return line.map(
+          v =>
+            this.normalizeValue(v, minX, maxX, 0, canvasWidth, false) / this.dpr
+        );
+      }
+      return line.map(
+        v =>
+          this.normalizeValue(v, minY, maxY, 0, canvasHeight, true) / this.dpr
       );
     });
   }
@@ -165,47 +244,101 @@ class Chart {
    * @return {number}
    * */
   normalizeValue(value, minValue, maxValue, screenMin, screenMax, isY) {
-    const coordinate = Math.round(
-      (value / (maxValue - minValue)) * (screenMax - screenMin),
-    );
+    const pixel = (maxValue - minValue) / (screenMax - screenMin);
+    const coordinate = (value - minValue) / pixel;
     return isY ? screenMax - coordinate : coordinate;
   }
 
   render() {
     requestAnimationFrame(() => {
-      context.clearRect(
+      this.context.clearRect(
         0,
         0,
         this.state.mainCanvasWidth,
-        this.state.mainCanvasHeight,
+        this.state.mainCanvasHeight
       );
 
       const normalizedLinesMain = this.normalizeEntry(
+        this.state,
         this.state.mainCanvasWidth,
         this.state.mainCanvasHeight,
-        this.state.linesMain,
+        false
       );
-      this.renderLines(normalizedLinesMain);
+      this.renderLines(this.context, normalizedLinesMain, 2);
+
+      this.contextLegend.clearRect(
+        0,
+        0,
+        this.state.legendCanvasWidth,
+        this.state.legendCanvasHeight
+      );
 
       const normalizedLinesLegend = this.normalizeEntry(
-        this.state.mainCanvasWidth,
-        this.state.mainCanvasHeight,
-        this.state.linesMain,
+        this.state,
+        this.state.legendCanvasWidth,
+        this.state.legendCanvasHeight,
+        true
       );
-      this.renderLines(normalizedLinesLegend);
+      this.renderLines(this.contextLegend, normalizedLinesLegend, 1);
+
+      this.renderWindow();
 
       if (this.checkRender()) {
         this.render();
       }
+      this.state.shouldRender = true;
     });
   }
 
-  renderLines(context, lines) {
+  renderWindow() {
+    const state = this.state;
+    const lines = state.lines;
+    const minX = state.minX;
+
+    const maxX = state.maxX;
+    const maxY = state.maxY;
+    const minY = state.minY;
+
+    const minXPixel = this.normalizeValue(
+      minX,
+      this.state.minXOrig,
+      this.state.maxXOrig,
+      0,
+      this.state.legendWidth,
+      false
+    );
+    const maxXPixel = this.normalizeValue(
+      maxX,
+      this.state.minXOrig,
+      this.state.maxXOrig,
+      0,
+      this.state.legendWidth,
+      false
+    );
+
+    const gripLeftPos = minXPixel;
+    this.gripLeft.style = `left:${gripLeftPos}px`;
+    const gripRightPos = maxXPixel - this.state.gripWidth;
+    this.gripRight.style = `left:${gripRightPos}px`;
+
+    this.glassLeft.style = `width:${gripLeftPos}px`;
+    const glassRightPos = gripRightPos + this.state.gripWidth;
+    this.glassRight.style = `left:${glassRightPos}px;`;
+
+    const style = `left:${gripLeftPos + this.state.gripWidth}px;width:${this
+      .state.legendWidth -
+      (gripLeftPos +
+        this.state.gripWidth +
+        (this.state.legendWidth - gripRightPos))}px`;
+    this.window.style = style;
+  }
+
+  renderLines(context, lines, lineWidth) {
     const linesNumber = lines.length;
     for (let counter = 1; counter < linesNumber; counter++) {
+      context.lineWidth = lineWidth;
       context.beginPath();
       context.strokeStyle = this.state.colors[counter - 1];
-      context.strokeWidth = 2;
       lines[counter].forEach((y, index) => {
         context.lineTo(lines[0][index], y);
       });
@@ -214,25 +347,32 @@ class Chart {
   }
 
   checkRender() {
-    return true;
+    return this.state.shouldRender;
   }
 }
 
-/** @typedef {{
-      linesMain: Array.<Array.<number>>,
-      linesLegend: Array.<Array.<number>>,
+new Array(5).fill(1).forEach((value, counter) => {
+  const chart = new Chart(counter);
+  window.addEventListener('DOMContentLoaded', () => chart.onLoad(), false);
+  window.addEventListener('unload', () => chart.onUnLoad(), false);
+});
 
-      minXMain: number,
-      maxXMain: number,
-      minYMain: number,
-      maxYMain: number,
+/** @typedef {{
+      shouldRender: boolean,
+
+      lines: !Array.<!Array.<number>>,
+
+      minX: number,
+      maxX: number,
+      minY: number,
+      maxY: number,
 
       minXLegend: number,
       maxXLegend: number,
       minYLegend: number,
       maxYLegend: number,
 
-      colors: Array.<string>
+      colors: !Array.<string>
 
 }} */
 const State = {};
